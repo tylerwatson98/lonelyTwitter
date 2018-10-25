f5b50e7fc50a4034b47868548b43196b0cc6cd6c
tylerwatson98 <32936743+tylerwatson98@users.noreply.github.com>
2018-10-25 17:34:05 -0600

    adding all

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
@@ -1,21 +1,11 @@
 package ca.ualberta.cs.lonelytwitter;
 
-import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
 
-import junit.framework.TestCase;
+public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2 <LonelyTwitterActivity>{
 
-/**
- * Created by wz on 14/09/15.
- */
-public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2 {
-
-    public LonelyTwitterActivityTest() {
-        super(ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity.class);
+    public LonelyTwitterActivityTest(){
+        super(LonelyTwitterActivity.class);
     }
 
-    public void testStart() throws Exception {
-        Activity activity = getActivity();
-
-    }
 }
