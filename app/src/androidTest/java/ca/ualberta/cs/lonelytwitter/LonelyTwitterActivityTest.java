3d327c2c47ab8089a866206d0ad5168340247b80
tylerwatson98 <32936743+tylerwatson98@users.noreply.github.com>
2018-10-25 17:45:05 -0600

    adding new

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
@@ -2,10 +2,26 @@ package ca.ualberta.cs.lonelytwitter;
 
 import android.test.ActivityInstrumentationTestCase2;
 
+import com.robotium.solo.Solo;
+
 public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2 <LonelyTwitterActivity>{
 
+    private Solo solo;
     public LonelyTwitterActivityTest(){
         super(LonelyTwitterActivity.class);
     }
 
+    @Override
+    public void setUp() {
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    @Override
+    public void tearDown(){
+        solo.finishOpenedActivities();
+    }
+
+    public void testTweet(){
+        solo.assertCurrentActivity("Wrong Acitivty","ABC");
+    }
 }
